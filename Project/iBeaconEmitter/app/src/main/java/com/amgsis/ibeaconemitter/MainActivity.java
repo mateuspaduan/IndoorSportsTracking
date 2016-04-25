d346fb9bbb9cf4618bed3bfb41aa00ae2034b44e
Lucas Selani de Melo <lucas_selani@hotmail.com>
2016-04-25 13:51:09 -0300

    Update MainActivity.java

--- Project/iBeaconEmitter/app/src/main/java/com/amgsis/ibeaconemitter/MainActivity.java
+++ Project/iBeaconEmitter/app/src/main/java/com/amgsis/ibeaconemitter/MainActivity.java
@@ -26,6 +26,30 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        
+        setAdvertiseData();
+        setAdvertiseSettings();
+        setAdvertiseCallback();
+    }
+    
+    private void setAdvertiseData(){
+        AdvertiseData.Builder mBuilder = new AdvertiseData.Builder();
+        ByteBuffer manufacturerData = ByteBuffer.allocate(24);
+        byte[] uuid = getIdAsByte(UUID.fromString("0CF052C2-97CA-407C-84F8-B62AAC4E9020"));
+        manufacturerData.put(0, (byte) 0x02); //FIRST BYTE iBeacon
+        manufacturerData.put(1, (byte) 0x15); //SECOND BYTE iBeacon
+        for(int i=2; i<18; i++) manufacturerData.put(i, uuid[i-2]); //UUID
+        manufacturerData.put(18, (byte));
+        manufacturerData.put(18, (byte));
+        manufacturerData.put(18, (byte));
+    }
+    
+    private byte[] getIdAsByte(UUID uuid)
+    {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+        return bb.array();
     }
 
     @Override
